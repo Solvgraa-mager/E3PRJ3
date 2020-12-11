0d0e16d280f5fe9d975982499089fec341a68e8a
Frederiklassen <70856541+Frederiklassen@users.noreply.github.com>
2020-12-11 19:30:55 +0100

    SumoBot dirSpeed classe

---
+++ Software/SumoBot Microcontroller/Has_Ret_Test/Header.h
@@ -0,0 +1,18 @@
+#pragma once
+#include <iostream>
+#include <string.h>
+#include<stdio.h>
+#include <math.h>
+#include "Header.h"
+
+using namespace std;
+
+
+class Class_Motor
+{
+public:
+	Class_Motor();
+	void setDirSpeed(char* C1);
+private:
+};
+
