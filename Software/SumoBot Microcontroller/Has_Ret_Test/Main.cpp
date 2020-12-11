fad138eee38282ac79eb6ee88cb041c1dc7ed3dc
Frederiklassen <70856541+Frederiklassen@users.noreply.github.com>
2020-12-11 15:56:39 +0100

    SumoBot Retning og Hastighed decoder færdig

--- Software/SumoBot Microcontroller/Has_Ret_Test/Main.cpp
+++ Software/SumoBot Microcontroller/Has_Ret_Test/Main.cpp
@@ -5,52 +5,47 @@
 
 using namespace std;
 
-
+    // const char C1[] = {'-','7','7','|','-','4','5', '\0' }; 
+	// const char C1[] = {'7','|', '-','4','5', '\0' };
+	 const char C1[] = { '-','1','0','0','|', '-','7','0', '\0' };
+    // const char C1[] = { '1','0','0','|','4','5', '\0' };
+	// const char C1[] = { '0','|','0', '\0' };
 int main()
 {
-	int Derection;
-	int PWM_Size = 0;
-	//const char C1[] = {'-','7','7','|', '-','4','5', '\0' };
-	//const char C1[] = {'7','|', '-','4','5', '\0' };
+	int Derection = 0; int PWM_Size = 0; int ValueDerection = 0; int Angle_Size = 0; int DerectionAngle = 0; int ValueAngle = 0; // Sætter værdier til 0
 
-	char C1[] = { '-','9','9','1','|', '-','4','5', '\0' };
-	//const char C1[] = {'1','0','0','|', '-','4','5', '\0' };
-
-	if (C1[0] == '-') { Derection = 1; }
+	if (C1[0] == '-') { Derection = 1; }                        // Checker om der er negativ fortegn og sætter "Derection" derefter
 	else { Derection = 0; }
 
-	int ValueDerection = 0; //= (int)C1[0 + Derection] - '0';
-
-	for (int i = 0 + Derection; i < 3+Derection; i++)
-	{
+	for (int i = 0 + Derection; i < 3 + Derection; i++)         // Løber Char arrayet igennem og checker antal pladser der er før "|"
+	{															// Og tiøfæjer vlrdien til "PWM_Size"
 		PWM_Size++;
-		if (C1[i] == '|')
-		{
-			i = 5;
-			PWM_Size--;
-		}
-		ValueDerection = (ValueDerection * 10) + ((int)C1[i] - '0');
+
+		if (C1[i] == '|')										// Breaker hvis 
+		{i = 5; PWM_Size--;}
 	}
 
-	if (Derection == 1) { ValueDerection = ValueDerection * (-1);}
+	if (C1[PWM_Size + 1 + Derection] == '-') { DerectionAngle = 1; }         // Checker om der er negativ fortegn og sætter "Derection" derefter
+	else { DerectionAngle = 0; }
 
-	cout << "Derection = " << Derection << endl;
-	cout << "PWM_Zize = " << PWM_Size << endl;
-	cout << ValueDerection << endl;
+	Angle_Size = sizeof(C1) - PWM_Size - Derection - DerectionAngle - 2;            // Beregner størelsen af Angle_Size
 
-	//cout << "ValuePWM = " << ValuePWM;
+	for (int i = sizeof(C1) - Angle_Size - 1 ; i < sizeof(C1)-1; i++)   // Omregner Char til int og lægger værduen ind i ValueAngle
+	{ValueAngle = (ValueAngle * 10) + ((int)C1[i] - '0');}
 
-	//if (C1[0] == '-') { Derection = 1; }
-	//else { Derection = 0; }
-	//int Value = (int)C1[0+Derection] - 48;
-	//int ValuePWM = (int)C1[0 + Derection] - 48;
-	//int size_Char = sizeof(C1);
-	//cout << size_Char << endl;
-    //(ValueDerection)+((int)C1[i] - 48);
-	//if (C1[i] == '|')
-	//{
-	//	 ValueDerection = ValueDerection + ((int)C1[i] - 48);
-	//	cout << "hej";
-	
+
+	for (int i = 0 + Derection; i < PWM_Size + Derection; i++)          // Omregner Char til int og lægger værduen ind i ValueDerection
+	{ValueDerection = (ValueDerection * 10) + ((int)C1[i] - '0');}
+
+
+	if (Derection == 1) { ValueDerection = ValueDerection * (-1);}     // Ganger med -1 for at få negativ fortegn hvis der er negativ fortegn i "Derection"
+	if (DerectionAngle == 1) { ValueAngle = ValueAngle * (-1);}        // Ganger med -1 for at få negativ fortegn hvis der er negativ fortegn i "DerectionAngle"
+
+
+	cout << ValueDerection << endl; 
+	cout << ValueAngle << endl;
+
+
+
 	return 0;
 }
