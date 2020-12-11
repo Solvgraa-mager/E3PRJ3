d2d801f4b4a8b7ad31de96b5c5344b0cba5a9b89
Frederiklassen <70856541+Frederiklassen@users.noreply.github.com>
2020-12-11 18:46:07 +0100

    Færdig udgave af retning/hastigheds funktion til SumoBot

--- Software/SumoBot Microcontroller/Has_Ret_Test/Main.cpp
+++ Software/SumoBot Microcontroller/Has_Ret_Test/Main.cpp
@@ -1,21 +1,26 @@
 #include <iostream>
 #include <string.h>
 #include<stdio.h>
+#include <math.h>
 
+#define PI 3.14159265
 
 using namespace std;
 
-    // const char C1[] = {'-','7','7','|','-','4','5', '\0' }; 
+     const char C1[] = {'-','7','7','|','-','4','5', '\0' }; 
 	// const char C1[] = {'7','|', '-','4','5', '\0' };
-	 const char C1[] = { '-','1','0','0','|', '-','7','0', '\0' };
-    // const char C1[] = { '1','0','0','|','4','5', '\0' };
+	// const char C1[] = { '-','1','0','0','|', '-','7','0', '\0' };
+    // const char C1[] = { '1','0','0','|','1','0', '\0' };
 	// const char C1[] = { '0','|','0', '\0' };
+
 int main()
 {
-	int Derection = 0; int PWM_Size = 0; int ValueDerection = 0; int Angle_Size = 0; int DerectionAngle = 0; int ValueAngle = 0; // Sætter værdier til 0
+	int Derection = 0; int PWM_Size = 0; int ValueDerection = 0; int Angle_Size = 0;     // Sætter værdier til default
+	int DerectionAngle = 0; int ValueAngle = 0; int MotorV ; int MotorH ;
+	int Input1, Input2, Input3, Input4;
 
-	if (C1[0] == '-') { Derection = 1; }                        // Checker om der er negativ fortegn og sætter "Derection" derefter
-	else { Derection = 0; }
+	if (C1[0] == '-') { Derection = 1; Input1 = 1; Input3 = 1; Input2 = 0; Input4 = 0; }     // Checker om der er negativ fortegn og sætter "Derection" derefter
+	else {Derection = 0; Input1 = 0; Input3 = 0; Input2 = 1; Input4 = 1;}
 
 	for (int i = 0 + Derection; i < 3 + Derection; i++)         // Løber Char arrayet igennem og checker antal pladser der er før "|"
 	{															// Og tiøfæjer vlrdien til "PWM_Size"
@@ -37,15 +42,28 @@ int main()
 	for (int i = 0 + Derection; i < PWM_Size + Derection; i++)          // Omregner Char til int og lægger værduen ind i ValueDerection
 	{ValueDerection = (ValueDerection * 10) + ((int)C1[i] - '0');}
 
+	if (ValueAngle != 0)
+	{
+		if (ValueAngle < 0) {
+			MotorH = ValueDerection;
+			MotorV = ValueDerection * cos(ValueAngle * PI / 180);
+		}
+		else {
+			MotorV = ValueDerection;
+			MotorH = ValueDerection * cos(ValueAngle * PI / 180);
+		}
+	}
+	else { MotorH = ValueDerection; MotorV = ValueDerection;}
 
-	if (Derection == 1) { ValueDerection = ValueDerection * (-1);}     // Ganger med -1 for at få negativ fortegn hvis der er negativ fortegn i "Derection"
-	if (DerectionAngle == 1) { ValueAngle = ValueAngle * (-1);}        // Ganger med -1 for at få negativ fortegn hvis der er negativ fortegn i "DerectionAngle"
 
-
-	cout << ValueDerection << endl; 
-	cout << ValueAngle << endl;
-
-
+	cout << "Input C1: " << C1 << endl;
+	cout <<"MotorV PWM %: " << MotorV << endl;
+	cout <<"MotorH PWM %: " << MotorH << endl;
+	cout << "Derection  'Frem = 0, Tilbage = 1' : " <<  Derection << endl;
+	cout << "Input 1: " << Input1 << endl;
+	cout << "Input 2: " << Input2 << endl;
+	cout << "Input 3: " << Input3 << endl;
+	cout << "Input 4: " << Input4 << endl;
 
 	return 0;
 }
