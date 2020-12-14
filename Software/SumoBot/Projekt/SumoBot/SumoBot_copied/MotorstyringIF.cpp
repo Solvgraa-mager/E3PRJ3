88a31f4994a526d2d0990e994d47cfba110771b1
Frederiklassen <70856541+Frederiklassen@users.noreply.github.com>
2020-12-14 22:13:41 +0100

    CodeComment

--- Software/SumoBot/Projekt/SumoBot/SumoBot_copied/MotorstyringIF.cpp
+++ Software/SumoBot/Projekt/SumoBot/SumoBot_copied/MotorstyringIF.cpp
@@ -26,54 +26,51 @@ void MotorstyringIF::setSpeedDir(char *speedDir)
 		int DerectionAngle = 0; int ValueAngle = 0; int MotorV; int MotorH;
 		int Input1, Input2, Input3, Input4;
 
-		if (speedDir[0] == '-') { Derection = 1; Input1 = 1; Input3 = 1; Input2 = 0; Input4 = 0; }     // Checker om der er negativ fortegn og sætter "Derection" derefter
-		else { Derection = 0; Input1 = 0; Input3 = 0; Input2 = 1; Input4 = 1; }
+		if (speedDir[0] == '-') { Derection = 1; Input1 = 1; Input3 = 1; Input2 = 0; Input4 = 0; }     // Checker om der er negativ eller positiv fortegn og sætter "Derection" derefter
+		else { Derection = 0; Input1 = 0; Input3 = 0; Input2 = 1; Input4 = 1; }      
 
 		for (int i = 0 + Derection; i < 3 + Derection; i++)         // Løber Char arrayet igennem og checker antal pladser der er før "|"
-		{															// Og tiøfæjer vlrdien til "PWM_Size"
+		{															// Og tiøfæjer værdien af dette til "PWM_Size"
 			PWM_Size++;
 
-			if (speedDir[i] == '|')										// Breaker hvis 
+			if (speedDir[i] == '|')										// Breaker hvis loopet møder "|" og trækker denne plads fra PWM_Size da "|" ikke skal tælles med
 			{
 				i = 5; PWM_Size--;
 			}
 		}
 
-		if (speedDir[PWM_Size + 1 + Derection] == '-') { DerectionAngle = 1; }         // Checker om der er negativ fortegn og sætter "Derection" derefter
+		if (speedDir[PWM_Size + 1 + Derection] == '-') { DerectionAngle = 1; }         // Checker om der er negativ fortegn og sætter "DerectionAngle" derefter
 		else { DerectionAngle = 0; }
 
 		Angle_Size = sizeof(speedDir) - PWM_Size - Derection - DerectionAngle - 2;            // Beregner størelsen af Angle_Size
 
-		for (int i = sizeof(speedDir) - Angle_Size - 1; i < sizeof(speedDir) - 1; i++)   // Omregner Char til int og lægger værduen ind i ValueAngle
-		{
-			ValueAngle = (ValueAngle * 10) + ((int)speedDir[i] - '0');
-		}
+		for (int i = sizeof(speedDir) - Angle_Size - 1; i < sizeof(speedDir) - 1; i++)   // Omregner Char til int og lægger værdien ind i ValueAngle
+		{ValueAngle = (ValueAngle * 10) + ((int)speedDir[i] - '0');}
 
 
-		for (int i = 0 + Derection; i < PWM_Size + Derection; i++)          // Omregner Char til int og lægger værduen ind i ValueDerection
-		{
-			ValueDerection = (ValueDerection * 10) + ((int)speedDir[i] - '0');
-		}
+		for (int i = 0 + Derection; i < PWM_Size + Derection; i++)          // Omregner Char til int og lægger værdien ind i ValueDerection
+		{ValueDerection = (ValueDerection * 10) + ((int)speedDir[i] - '0');}
 
-		if (ValueAngle != 0)
+
+		if (ValueAngle != 0)					// Hvis "Angle" er alt andet end 0 = "Bilen kører ikke ligeud" sættes motorenes PWM efter dette
 		{
-			if (ValueAngle < 0) {
+			if (ValueAngle < 0) {			// Bilen kører med - grader = "SumoBotten kører mod venstre"
 				MotorH = ValueDerection;
-				MotorV = ValueDerection * cos(ValueAngle * PI / 180);
+				MotorV = ValueDerection * cos(ValueAngle * PI / 180); // PWM justering udregnes efter vinklen
 			}
-			else {
+			else {							// Bileb kører med + grader = "SumoBotten kører mod "højre"
 				MotorV = ValueDerection;
-				MotorH = ValueDerection * cos(ValueAngle * PI / 180);
+				MotorH = ValueDerection * cos(ValueAngle * PI / 180); // PWM justering udregnes efter vinklen
 			}
 		}
-		else { MotorH = ValueDerection; MotorV = ValueDerection; }
+		else { MotorH = ValueDerection; MotorV = ValueDerection; } // Hvis angleDerection er = 0 "SumoBotten kører ligeud" sættes begge motorenes PWM til værdien af hastigheden
 
-		digitalWrite(22, Input1);
+		digitalWrite(22, Input1); // input pindene sættes så motorstyringen ved hvilken retning den skal køre
 		digitalWrite(21, Input2);
 		digitalWrite(4, Input3);
 		digitalWrite(5, Input4);
 
-		pwmWrite(26, MotorV*10);
-		pwmWrite(23, MotorH*10);
+		pwmWrite(26, MotorV*10); // PWM pindene sættes og ganges med 10 da "WiringPie" har maks PWM på 1024 og der anvendes maks pwn på 100 i koden. (100 * 10 = 1000) så
+		pwmWrite(23, MotorH*10); // Så maks PWM er 24 under det maksimale mulige.
 };
 /// setSpeedDirStop
