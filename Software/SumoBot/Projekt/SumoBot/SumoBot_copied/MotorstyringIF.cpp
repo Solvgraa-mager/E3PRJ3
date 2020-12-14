92ec831adc86ede66f2c9277473e2261a1b3baf0
Frederiklassen <70856541+Frederiklassen@users.noreply.github.com>
2020-12-14 21:44:56 +0100

    speedDir

--- Software/SumoBot/Projekt/SumoBot/SumoBot_copied/MotorstyringIF.cpp
+++ Software/SumoBot/Projekt/SumoBot/SumoBot_copied/MotorstyringIF.cpp
@@ -20,39 +20,39 @@ MotorstyringIF::MotorstyringIF()
 /// MotorstyringIFStop
 
 /// setSpeedDirStart
-void MotorstyringIF::setSpeedDir(char *C1)
+void MotorstyringIF::setSpeedDir(char *speedDir)
 {
 		int Derection = 0; int PWM_Size = 0; int ValueDerection = 0; int Angle_Size = 0;     // Sætter værdier til default
 		int DerectionAngle = 0; int ValueAngle = 0; int MotorV; int MotorH;
 		int Input1, Input2, Input3, Input4;
 
-		if (C1[0] == '-') { Derection = 1; Input1 = 1; Input3 = 1; Input2 = 0; Input4 = 0; }     // Checker om der er negativ fortegn og sætter "Derection" derefter
+		if (speedDir[0] == '-') { Derection = 1; Input1 = 1; Input3 = 1; Input2 = 0; Input4 = 0; }     // Checker om der er negativ fortegn og sætter "Derection" derefter
 		else { Derection = 0; Input1 = 0; Input3 = 0; Input2 = 1; Input4 = 1; }
 
 		for (int i = 0 + Derection; i < 3 + Derection; i++)         // Løber Char arrayet igennem og checker antal pladser der er før "|"
 		{															// Og tiøfæjer vlrdien til "PWM_Size"
 			PWM_Size++;
 
-			if (C1[i] == '|')										// Breaker hvis 
+			if (speedDir[i] == '|')										// Breaker hvis 
 			{
 				i = 5; PWM_Size--;
 			}
 		}
 
-		if (C1[PWM_Size + 1 + Derection] == '-') { DerectionAngle = 1; }         // Checker om der er negativ fortegn og sætter "Derection" derefter
+		if (speedDir[PWM_Size + 1 + Derection] == '-') { DerectionAngle = 1; }         // Checker om der er negativ fortegn og sætter "Derection" derefter
 		else { DerectionAngle = 0; }
 
-		Angle_Size = sizeof(C1) - PWM_Size - Derection - DerectionAngle - 2;            // Beregner størelsen af Angle_Size
+		Angle_Size = sizeof(speedDir) - PWM_Size - Derection - DerectionAngle - 2;            // Beregner størelsen af Angle_Size
 
-		for (int i = sizeof(C1) - Angle_Size - 1; i < sizeof(C1) - 1; i++)   // Omregner Char til int og lægger værduen ind i ValueAngle
+		for (int i = sizeof(speedDir) - Angle_Size - 1; i < sizeof(speedDir) - 1; i++)   // Omregner Char til int og lægger værduen ind i ValueAngle
 		{
-			ValueAngle = (ValueAngle * 10) + ((int)C1[i] - '0');
+			ValueAngle = (ValueAngle * 10) + ((int)speedDir[i] - '0');
 		}
 
 
 		for (int i = 0 + Derection; i < PWM_Size + Derection; i++)          // Omregner Char til int og lægger værduen ind i ValueDerection
 		{
-			ValueDerection = (ValueDerection * 10) + ((int)C1[i] - '0');
+			ValueDerection = (ValueDerection * 10) + ((int)speedDir[i] - '0');
 		}
 
 		if (ValueAngle != 0)
