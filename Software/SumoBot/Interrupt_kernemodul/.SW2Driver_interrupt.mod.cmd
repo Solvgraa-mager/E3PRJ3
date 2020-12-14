f224964f055e97346b8cbcb758ab6f684bc34f85
Your Name <you@example.com>
2020-12-14 20:50:21 +0100

    add of interrupt SumoBot

---
+++ Software/SumoBot/Interrupt_kernemodul/.SW2Driver_interrupt.mod.cmd
@@ -0,0 +1,1 @@
+cmd_/home/stud/SumoBot_Battle/E3PRJ3/Software/SumoBot/Interrupt_kernemodul/SW2Driver_interrupt.mod := { echo  /home/stud/SumoBot_Battle/E3PRJ3/Software/SumoBot/Interrupt_kernemodul/SW2Driver_interrupt.o; echo; } > /home/stud/SumoBot_Battle/E3PRJ3/Software/SumoBot/Interrupt_kernemodul/SW2Driver_interrupt.mod
